49306a75821b11d389cb864e35fb04c8efa24205
shiva-yadav-9390 <jshiva38787@gmail.com>
2026-04-08 20:39:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get input values from form
+        features = [float(x) for x in request.form.values()]
+        final_input = np.array([features])
+
+        prediction = model.predict(final_input)
+
+        if prediction[0] == 1:
+            result = "Heart Disease Detected"
+        else:
+            result = "No Heart Disease"
+
+        return render_template('index.html', prediction_text=result)
+
+    except:
+        return render_template('index.html', prediction_text="Error in input")
+
+if __name__ == "__main__":
+    app.run(debug=True)
